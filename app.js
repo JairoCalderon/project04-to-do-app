8c8343f588569ff556df937dad3782e74f9acac9
JairoCalderon <j.calderoneng@gmail.com>
2022-07-20 15:34:59 -0400

    phase 1 completed

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
-const campo = document.querySelector("#toDoItem")
+//display a form where users can add tasks
+//add new task on the form
+//once submitted, grab the value from the form and display as a list on the ul
+//when user click on a task, the task should provide a checkbox
+//once the checkbox of a task is clicked the task will become grey to inform that the task has been completed
+
+
+const formElement = document.querySelector("form");
+const taskList = document.querySelector("ul")
+
+
+formElement.addEventListener("submit", function (e) {
+    //preventing the default bahaviour of forms
+    e.preventDefault();
+
+
+    const inputElement = document.querySelector("input")
+
+    //selecting the input value
+    const task = inputElement.value;
+
+
+    if (task === "") {
+        alert("Please insert a task")
+    } else {
+        const listTaskElmt = document.createElement("li");
+        listTaskElmt.innerHTML = `<i class="far fa-square"></i>`;
+
+
+        const paragphElemt = document.createElement("p");
+        paragphElemt.textContent = task;
+
+        listTaskElmt.appendChild(paragphElemt);
+        taskList.appendChild(listTaskElmt);
 
 
 
-const submeter = (e) => {
+        //Cleaning the input field
+        inputElement.value = ""
+    }
 
-    campo.addEventListener("submit", console.log("submited"))
+});
 
-}
+document.querySelector("li").addEventListener("click", function () {
+    console.log("item clicked");
+})
+
+
